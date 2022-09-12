349ffe2edb7c08b18cd9424d9748dc4510c53bd1
waltersmatthew <waltersmatthew88@gmail.com>
2022-09-12 14:47:11 -0700

    step 4

--- controllers/users.js
+++ controllers/users.js
@@ -15,7 +15,7 @@ router.post('/', async (req,res)=>{
         // res.cookie('key', value)
         res.cookie('userId', newUser.id)
         //redirect to the homepage
-        res.redirect('/')
+        res.redirect('/users/profile')
     } catch(err) {
         console.log(err)
         res.send('server error')
@@ -24,16 +24,61 @@ router.post('/', async (req,res)=>{
 
 // GET /users/login -- show a login form to the user
 router.get('/login', (req,res) =>{
-    res.render('users/login.ejs')
+    console.log(req.query)
+    res.render('users/login.ejs', {
+        //if req.query message exists, pass the message, otherwise pass in null
+        //ternary operator
+        //condition ? expression if truthy : expression if falsy
+        message: req.query.message ? req.query.message : null
+    })
 })
 // POST /users/login -- accept a payload of form data and use it to log a user in
-router.post('/login', (req,res)=>{
-    console.log(req.body)
-    res.send('log the user in')
+router.post('/login', async (req,res)=>{
+    try{
+        const user = await db.user.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        const noLoginMessage = 'Incorrect username or password'
+        // look up user in the db using spplied email
+        //if user is not found -- send them back to login form
+        if(!user) {
+            console.log('user not found')
+            res.redirect('/users/login?message=' + noLoginMessage)
+        //if the user is found but has wrong password -- send back to login form
+        } else if (user.password !== req.body.password){
+            console.log('wrong password')
+            res.redirect('/users/login?message=' + noLoginMessage)
+        //if user is found and supplied pw matches what's in the db -- log in
+        }else {
+            console.log('logging the user in')
+            res.cookie('userId', user.id)
+            res.redirect('/users/profile')
+        }
+    }catch(err){
+        console.log(err)
+        res.send('server error')
+    }
 })
 
 // GET /users/logout -- log out a user by clearing the stored cookie
 router.get('/logout', (req,res) =>{
-    res.send('log the user out')
+    // clear the cookie
+    res.clearCookie('userId')
+    //redirect to home page
+    res.redirect('/')
 })
+
+router.get('/profile', (req,res)=>{
+    //if user is not logged...redirect
+    if(!res.locals.user){
+        res.redirect('/users/login?message=You must authenticate before you are authorized to view this resource')
+    }else{
+        res.render('users/profile.ejs', {
+            user: res.locals.user
+        })
+    }
+})
+
 module.exports = router
--- index.js
+++ index.js
@@ -2,17 +2,37 @@
 require('dotenv').config()
 const express = require('express')
 const ejsLayouts = require('express-ejs-layouts')
-
+const cookieParser = require('cookie-parser')
+const db = require('./models')
 //config express app/middlewares
 const app = express()
 const PORT = process.env.PORT || 3000
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
 app.use(express.urlencoded({ extended: false}))
+app.use(cookieParser())
+//our custom auth middleware
+app.use(async (req,res,next) =>{
+    // console.log('hello from a middleware')
+    res.locals.myData = "hello, route"
+    //if there is a cookie on the incoming request
+    if(req.cookies.userId) {
+        //look up the user in the db
+        const user = await db.user.findByPk(req.cookies.userId)
+        //mount the user on res.locals
+        res.locals.user = user
+    // if there is no cookie -- set the user to be null in the res.locals
+    }else{
+        res.locals.user = null
+    }
 
-
+    // move on to the next route or middleware in the chain
+    next()
+})
 // route definitions
 app.get('/', (req,res)=>{
+    // console.log('incoming cookie 🍪', req.cookies)
+    console.log('the currently logged in user is:', res.locals.user)
     res.render('home.ejs')
 })
 
