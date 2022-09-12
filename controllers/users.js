244eb7a92c819216b7b5d07906f5a8bcf3e7a8bb
waltersmatthew <waltersmatthew88@gmail.com>
2022-09-12 12:28:42 -0700

    add login view and get req.body

--- controllers/users.js
+++ controllers/users.js
@@ -7,8 +7,6 @@ const db = require('../models')
 router.get('/new', (req,res) =>{
     res.render('users/new.ejs')
 })
-
-
 // POST /users -- create a new user in the db
 router.post('/', async (req,res)=>{
     try {
@@ -24,5 +22,18 @@ router.post('/', async (req,res)=>{
     }
 })
 
+// GET /users/login -- show a login form to the user
+router.get('/login', (req,res) =>{
+    res.render('users/login.ejs')
+})
+// POST /users/login -- accept a payload of form data and use it to log a user in
+router.post('/login', (req,res)=>{
+    console.log(req.body)
+    res.send('log the user in')
+})
 
+// GET /users/logout -- log out a user by clearing the stored cookie
+router.get('/logout', (req,res) =>{
+    res.send('log the user out')
+})
 module.exports = router
